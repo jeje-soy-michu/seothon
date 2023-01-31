eebbb09f47fb861c8395bab92d83f0feefd3480f
Michu <97michuxd@gmail.com>
2023-01-31 20:43:55 +0000

    Add default theme

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -5,4 +5,8 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: ["forest", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 };
