81e3e965a87f159e1ccab51988c8ea83b1e590b5
Michu <97michuxd@gmail.com>
2023-02-04 21:16:37 +0000

    Add InputTextArea component

--- packages/website/src/components/analyzer/post.tsx
+++ packages/website/src/components/analyzer/post.tsx
@@ -1,11 +1,10 @@
 import { component$ } from '@builder.io/qwik'
 import { useAnalyzerContext } from '~/components/analyzer/hooks/useAnalyzerContext'
 
-export const InputTextArea = component$(() => {
+export const InputTextArea = component$(({isDisabled}: {isDisabled: boolean}) => {
   const context = useAnalyzerContext()
   return (
-    <div class="flex-row w-4/5 p-5">
-      <label for="text">Text to analyze</label>
+    <>
       <textarea
         class="textarea textarea-bordered textarea-lg w-full h-full"
         placeholder="Introduce your text to analyze"
@@ -15,11 +14,20 @@ export const InputTextArea = component$(() => {
           context.post = (event.target as HTMLTextAreaElement).value
         }}
         value={context.post}
+        disabled={isDisabled}
       />
-    </div>
+    </>
   )
 })
 
 export default component$(() => {
-  return <InputTextArea />
+  const {status} = useAnalyzerContext()
+
+  return (
+    <div class="flex-row w-4/5 p-5">
+      <label for="text">Text to analyze</label>
+      {status === "WAITING_FOR_INPUT" && <InputTextArea isDisabled={false} />}
+      {status === "ANALYZING" && <InputTextArea isDisabled={true} />}
+    </div>
+  )
 })
