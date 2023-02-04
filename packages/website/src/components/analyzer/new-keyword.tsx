75a2a21a27a79b42eba3426bbfe40da057d08354
Michu <97michuxd@gmail.com>
2023-02-04 17:20:45 +0000

    Add types to inputs in new keyword

--- packages/website/src/components/analyzer/new-keyword.tsx
+++ packages/website/src/components/analyzer/new-keyword.tsx
@@ -12,14 +12,14 @@ export default component$(() => {
         <label class="label">
           <span class="label-text">{"SEO Keyword (not empty)"}</span>
         </label>
-        <input type="text" placeholder="Type keyword" class="input input-bordered w-full max-w-xs" value={keyword.value} onInput$={e => {keyword.value = e.target.value}} />
+        <input type="text" placeholder="Type keyword" class="input input-bordered w-full max-w-xs" value={keyword.value} onInput$={e => {keyword.value = (e.target as HTMLInputElement).value}} />
       </div>
 
       <div class="form-control w-full max-w-xs">
         <label class="label">
           <span class="label-text">{"Search volume (volume > 0)"}</span>
         </label>
-        <input type="number" placeholder="Type volume" class="input input-bordered w-full max-w-xs" value={volume.value} onInput$={e => {volume.value = e.target.value}} />
+        <input type="number" placeholder="Type volume" class="input input-bordered w-full max-w-xs" value={volume.value} onInput$={e => {volume.value = Number((e.target as HTMLInputElement).value)}} />
       </div>
 
       <button class="btn" onClick$={() => {
