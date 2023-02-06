271d8005218e0ea5aa8cb826a8ba616c33562dc1
Michu <97michuxd@gmail.com>
2023-02-06 19:25:26 +0000

    Fix refactor buttons

--- packages/website/src/components/analyzer/index.tsx
+++ packages/website/src/components/analyzer/index.tsx
@@ -1,9 +1,9 @@
 import { component$ } from '@builder.io/qwik'
-import AnalyzeButton from '~/components/analyzer/analyze-button'
-import ClearButton from '~/components/analyzer/clear-button'
+import AnalyzeButton from '~/components/analyzer/buttons/analyze-button'
+import ClearButton from '~/components/analyzer/buttons/clear-button'
 import { useAnalyzerProvider } from '~/components/analyzer/hooks/useAnalyzerContext'
 import Keywords from '~/components/analyzer/keywords'
-import LoadDemoButton from '~/components/analyzer/load-demo-button'
+import LoadDemoButton from '~/components/analyzer/buttons/load-demo-button'
 import Post from '~/components/analyzer/post'
 
 export default component$(() => {
